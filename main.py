4589962b2b7a8955b6d30a78e22ceb1ccccd16b6
Julio <julio.mech@hotmail.com>
2023-06-20 15:26:55 -0300

    add function

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-
 import pyttsx3
 import speech_recognition as sr
 import datetime
+import wikipedia
 
 audio = sr.Recognizer()
 maquina = pyttsx3.init()
+
 
 def executaComando():
     try:
         with sr.Microphone() as source:
             print("Ouvindo...")
-            voz=audio.listen(source)
-            comando=audio.recognize_google(voz, language='pt-BR')
+            voz = audio.listen(source)
+            comando = audio.recognize_google(voz, language='pt-BR')
             comando = comando.lower()
-            if 'tina' in comando:
-                comando = comando.replace('tina', '')
+            if 'Robo' in comando:
+                comando = comando.replace('Robo', '')
                 maquina.say(comando)
                 maquina.runAndWait()
 
@@ -23,11 +24,19 @@ def executaComando():
 
     return comando
 
+
 def comandoVoiceUser():
     comando = executaComando()
     if 'horas' in comando:
         hora = datetime.datetime.now().strftime('%H:%M')
-        maquina.say('Agora são'+hora)
+        maquina.say('Agora são' + hora)
+        maquina.runAndWait()
+    elif 'procurar' in comando:
+        procure = comando.replace('procurar', ' ')
+        wikipedia.set_lang('pt')
+        result = wikipedia.summary(procure, 2)
+        print(result)
+        maquina.say(result)
         maquina.runAndWait()
 
 
